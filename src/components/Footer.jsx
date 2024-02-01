a312d3dda4a5a89de3af837df79a4e9f9bbbd776
omarlaneor <annielaneor@gmail.com>
2024-02-01 00:12:20 -0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+export function Footer() {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className="footer">
+      <h4>
+        Práctica Calificada 9 - FUNVAL 2024 ⚛️ － <span>omarlaneor</span>
+      </h4>
+      <h5>Shopping Cart using useContext & useReducer ✈ </h5>
+    </footer>
+  );
+}
